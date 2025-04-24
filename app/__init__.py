f4119b6d47942b6f44d6741d3cdc1d30e11d6ec2
Brynjólfur Georgsson <binnimar@Brynjolfurs-MacBook-Pro.local>
2025-04-24 00:33:19 +0000

    Simplified

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    
-    from .routes import main  # Import the blueprint
-    app.register_blueprint(main)
-    
-    return app
